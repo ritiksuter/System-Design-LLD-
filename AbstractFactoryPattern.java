a4da61afa76fa0826f666b8ed95efdcd2c08239f
ritiksuter <ritiksuteri@gmail.com>
2026-02-25 17:36:14 +0530

    Pattern

---
+++ AbstractFactoryPattern.java
@@ -0,0 +1,63 @@
+// Vehicle.java - Common Interface
+interface Vehicle {
+  void start();
+  void stop();
+}
+// Concrete Classes for Car Brands
+class Honda implements Vehicle {
+  public void start() {
+    System.out.println("Honda Car is starting");
+  }
+  public void stop() {
+    System.out.println("Honda Car is stopping");
+  }
+}
+class Toyota implements Vehicle {
+  public void start() {
+    System.out.println("Toyota Car is starting");
+  }
+  public void stop() {
+    System.out.println("Toyota Car is stopping");
+  }
+}
+class BMW implements Vehicle {
+  public void start() {
+    System.out.println("BMW Car is starting");
+  }
+  public void stop() {
+    System.out.println("BMW Car is stopping");
+  }
+}
+// Abstract Factory Interface
+interface VehicleFactory {
+  Vehicle createVehicle();
+}
+// Concrete Factories for Each Car Brand
+class HondaFactory implements VehicleFactory {
+  public Vehicle createVehicle() {
+    return new Honda();
+  }
+}
+class ToyotaFactory implements VehicleFactory {
+  public Vehicle createVehicle() {
+    return new Toyota();
+  }
+}
+class BMWFactory implements VehicleFactory {
+  public Vehicle createVehicle() {
+    return new BMW();
+  }
+}
+// Client Code
+public class AbstractFactoryPattern {
+  public static void main(String[] args) {
+    VehicleFactory hondaFactory = new HondaFactory();
+    Vehicle honda = hondaFactory.createVehicle();
+    honda.start();
+    honda.stop();
+    VehicleFactory toyotaFactory = new ToyotaFactory();
+    Vehicle toyota = toyotaFactory.createVehicle();
+    toyota.start();
+    toyota.stop();
+  }
+}
---
+++ FactoryDesignPattern.java
@@ -0,0 +1,57 @@
+interface Vehicle {
+    void start();
+
+    void stop();
+}
+
+class Car implements Vehicle {
+    public void start() {
+        System.out.println("Car is starting...");
+    }
+
+    public void stop() {
+        System.out.println("Car is stopping...");
+    }
+}
+
+class Truck implements Vehicle {
+    public void start() {
+        System.out.println("Truck is starting...");
+    }
+
+    public void stop() {
+        System.out.println("Truck is stopping...");
+    }
+}
+
+class Bike implements Vehicle {
+    public void start() {
+        System.out.println("Bike is starting...");
+    }
+
+    public void stop() {
+        System.out.println("Bike is stopping...");
+    }
+}
+
+
+
+// Vehicle Factory Method
+class VehicleFactory {
+    public static Vehicle getVehicle(String vehicleType) {
+        if (vehicleType.equals("Car")) return new Car();
+        else if (vehicleType.equals("Truck")) return new Truck();
+        else if (vehicleType.equals("Bike")) return new Bike();
+        else {
+            throw new IllegalArgumentException("Unknown Vehicle type...");
+        }
+    }
+}
+
+public class FactoryDesignPattern {
+    public static void main(String[] args) {
+        Vehicle v1 = VehicleFactory.getVehicle("Car");
+        v1.start();
+        v1.stop();
+    }
+}
